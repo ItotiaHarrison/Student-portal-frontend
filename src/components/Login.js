4e73829e7e91c4ea8667350ee1c3e8c5aab46692
ItotiaHarrison <harrison.itotia@student.moringaschool.com>
2022-11-08 15:02:46 +0300

    style page

--- src/components/Login.js
+++ src/components/Login.js
@@ -87,7 +87,7 @@ function Login() {
   return (
     <div className="app">
       <div className="image">
-        <img src="https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt='' width="600" height="500"></img>
+        <img src="https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt='' width="600" height="400"></img>
       </div>
     
       <div className="login-form">
